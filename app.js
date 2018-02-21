a6dada5ba09496357457480305f06e55cbf6e97f
Chu Hean <chuhean@gmail.com>
2018-02-21 05:52:17 +0000

    Setup and landing page

---
+++ app.js
@@ -0,0 +1,44 @@
+//======================================================
+//IMPORT MODULES
+//======================================================
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    flash           = require("connect-flash"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override");
+
+//======================================================
+//IMPORT ROUTES
+//======================================================
+var indexRoutes     = require("./routes/index");
+
+//======================================================
+//CONNECT APPJS TO MONGODB DATABASE
+//======================================================
+mongoose.connect("mongodb://localhost/socially");
+
+//======================================================
+//UTILIZE IMPORTED FUNCTIONS
+//======================================================
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+
+//======================================================
+//PASSPORTJS CONFIGURATION
+//======================================================
+
+
+//======================================================
+//UTILIZING ROUTES
+//======================================================
+
+
+//======================================================
+//INITIATE NODEJS TO START LISTENING REQUEST
+//======================================================
+app.listen(process.env.PORT, process.env.IP);
+
